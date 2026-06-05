c33bcbfda1794135381518ece36aa8b5e6c4ea24
jgj52 <email@jgj52.hu>
2026-06-05 16:56:56 +0200

    allow cors

---
+++ src/main/java/hu/jgj52/capeyapi/CorsConfig.java
@@ -0,0 +1,19 @@
+package hu.jgj52.capeyapi;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "https://capey.app",
+                        "http://localhost:3000"
+                )
+                .allowedMethods("*");
+    }
+}
